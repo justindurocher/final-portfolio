30e774ac8a5b0f17e8a9149e23cece1f2834b8bc
Justin Durocher <justduro@gmail.com>
2020-04-18 19:44:20 -0400

    navbar

--- final-portfolio/src/App.js
+++ final-portfolio/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import './App.css';
+
+import * as Bootstrap from 'react-bootstrap';
+
+
+
+
 
 function App() {
   return (
-     console.log("Hello World")
+    <Bootstrap.Navbar bg="info" variant="dark">
+    <Bootstrap.Navbar.Brand href="#home">Justin Durocher</Bootstrap.Navbar.Brand>
+    <Bootstrap.Nav className="mr-auto">
+      <Bootstrap.Nav.Link href="#home">Home</Bootstrap.Nav.Link>
+      <Bootstrap.Nav.Link href="#features">Features</Bootstrap.Nav.Link>
+      <Bootstrap.Nav.Link href="#pricing">Pricing</Bootstrap.Nav.Link>
+    </Bootstrap.Nav>
+    </Bootstrap.Navbar>
   );
 }
 
